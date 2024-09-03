9b16a0dbcf65d785ebbe8900155369a2acb61745
Quratulain Aqeel <aquratulain98@gmail.com>
2024-09-03 23:52:36 +0500

    Feature Prompting and metadata for the chstbot

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,13 +1,19 @@
+import { chatbotPrompt } from "@/app/helpers/constants/chatbot-prompt";
 import { ChatGPTMessage } from "@/lib/openai-stream";
 import { MessageArraySchema } from "@/lib/validators/message";
-import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const parsedMessages = MessageArraySchema.parse(messages);
 
-  const outboundMessages: ChatGPTMessage[];
+  const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => ({
+    role: message.isUserMessage ? "user" : "system",
+    content: message.text,
+  }));
 
-  return NextResponse.json({});
+  outboundMessages.unshift({
+    role: "system",
+    content: chatbotPrompt,
+  });
 }
--- src/lib/openai-stream.ts
+++ src/lib/openai-stream.ts
@@ -1,4 +1,5 @@
 export type ChatGPTAgent = "user" | "system";
+
 export interface ChatGPTMessage {
   role: ChatGPTAgent;
   content: string;
