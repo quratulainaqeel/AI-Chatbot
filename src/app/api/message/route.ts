cbab79c09f5141e1c66a55e3bd3a9c8f53d9f1f6
Quratulain Aqeel <aquratulain98@gmail.com>
2024-09-05 00:05:20 +0500

    Some updates

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -3,6 +3,7 @@ import { ChatGPTMessage } from "@/lib/openai-stream";
 import { MessageArraySchema } from "@/lib/validators/message";
 
 export async function POST(req: Request) {
+  console.log("hh")
   const { messages } = await req.json();
 
   const parsedMessages = MessageArraySchema.parse(messages);
